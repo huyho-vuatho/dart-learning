849763cde250296f7129d80ba84dbc8be4b49a4b
huyho-vuatho <huy.vietho@vuatho.com>
2024-08-16 20:27:15 +0700

    second commit

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -2,4 +2,5 @@ import 'package:dart_learning/dart_learning.dart' as dart_learning;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_learning.calculate()}!');
+  print('Hello world: ${dart_learning.calculate()}!');
 }
